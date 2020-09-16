39f559becd5ae163c27b365db4c25d3827fc2f4f
Miguel Manzano García <mmanzano@gmail.com>
2020-09-16 11:45:05 +0200

    Add get currencies to balance object

--- src/Objects/Balance.php
+++ src/Objects/Balance.php
@@ -28,27 +28,41 @@ class Balance
         $this->connect_reserved = $balance->connect_reserved;
         $this->livemode = $balance->livemode;
         $this->pending = $balance->pending;
-   }
+    }
 
-   public function getAvailable($currency)
-   {
-       $currency = strtolower($currency);
+    public function getCurrencies()
+    {
+        $pendingCurrencies = collect($this->pending)->pluck('currency')->unique()->values();
 
-       $availableBalance = collect($this->available)
-           ->whereIn('currency', [$currency, strtoupper($currency)])
-           ->first();
+        $availableCurrencies = collect($this->available)->pluck('currency')->unique()->values();
 
-       return $availableBalance->amount ?? null;
-   }
+        return $pendingCurrencies->merge($availableCurrencies)
+            ->map(function ($currency) {
+                return strtolower($currency);
+            })->unique()
+            ->sort()
+            ->values();
+    }
 
-   public function getPending($currency)
-   {
-       $currency = strtolower($currency);
+    public function getAvailable($currency)
+    {
+        $currency = strtolower($currency);
 
-       $pendingBalance = collect($this->pending)
-           ->whereIn('currency', [$currency, strtoupper($currency)])
-           ->first();
+        $availableBalance = collect($this->available)
+            ->whereIn('currency', [$currency, strtoupper($currency)])
+            ->first();
 
-       return $pendingBalance->amount ?? null;
-   }
+        return $availableBalance->amount ?? null;
+    }
+
+    public function getPending($currency)
+    {
+        $currency = strtolower($currency);
+
+        $pendingBalance = collect($this->pending)
+            ->whereIn('currency', [$currency, strtoupper($currency)])
+            ->first();
+
+        return $pendingBalance->amount ?? null;
+    }
 }
--- tests/PaymentProcessorBalanceTest.php
+++ tests/PaymentProcessorBalanceTest.php
@@ -17,6 +17,7 @@ class PaymentProcessorBalanceTest extends TestCase
         $this->assertEquals('R64\Stripe\Objects\Balance', get_class($balance));
         $this->assertEquals(1500, $balance->getAvailable('USD'));
         $this->assertEquals(1000, $balance->getPending('USD'));
+        $this->assertEquals(collect(['eur', 'usd']), $balance->getCurrencies());
     }
 
     /**
